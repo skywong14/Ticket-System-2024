5cf1204ed1f8816aeaa7e1ee81f6bceadcf0aaa2
skywong14 <skywong0014@gmail.com>
2024-05-31 10:16:14 +0800

    finish refund!

--- codes/tools/BPTree.hpp
+++ codes/tools/BPTree.hpp
@@ -15,7 +15,7 @@ using std::ofstream;
 using sjtu::vector;
 
 //each internal node with M keys and M+1 sons
-template<class T, int Max_Nodes = 3000, int M = 300, int Buffer_Size = 500>
+template<class T, int Max_Nodes = 3000, int M = 300, int Buffer_Size = 50>
 class BPTree{
 private:
     fstream file, file_value;
